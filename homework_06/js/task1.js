9aacbe2943d817cc8daea53dc4c2378d08273e4e
RomanCook <cookr@ukr.net>
2018-02-11 23:39:05 +0200

    hw06

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -4,26 +4,25 @@ a = prompt("Please enter lenghf 1-st side of triangle",0);
 b = prompt("Please enter lenghf 2-nd side of triangle",0);
 c = prompt("Please enter lenghf 3-rd side of triangle",0);
 
-if (a < 0 && b < 0 && c < 0 &&
-     a == null || b == null || c == null /* &&
-typeof(a) == string || typeof(b) == string || typeof(c) == string*/) {
+if (a < 0 && b < 0 && c < 0 || a == null || b == null || c == null || isNaN(a) || isNaN(b) || isNaN(c)){
     console.log('For data',a,b,c,'Incorrect data');
-    } else{
+    }else{
         if (a*a == +b*b + +c*c || b*b == +a*a + +c*c || c*c == +a*a + +b*b){
             TypeTriangle = "right triangle";
-        } else{
+        }else{
              if (a == b && b == c){
                 TypeTriangle = "scalene";
-            }
-            else{
+            }else{
                 if(a == b || b == c || c == a){
                     TypeTriangle = "isosceles";
-                }
-                else{
+                }else{
                     TypeTriangle = "scalene";
                 }
             }
         }
+    a = parseFloat(a);
+    b = parseFloat(b);
+    c = parseFloat(c);
     S = Math.sqrt((+a + +b - c)*(+a - b + +c)*(-a + +b + +c)*(+a + +b + +c))/4;
-    console.log('Type of triangle is',TypeTriangle,' and square is',S.toFixed(2));
+    console.log('Type of triangle is',TypeTriangle,'and square is',S.toFixed(2));
 }
