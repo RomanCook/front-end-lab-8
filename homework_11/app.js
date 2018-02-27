6380873ea6dfc855cd15272c5b05b3e016674d02
RomanCook <cookr@ukr.net>
2018-02-27 17:38:54 +0200

    hw11

---
+++ homework_11/app.js
@@ -0,0 +1,12 @@
+var rootNode = document.getElementById("root");
+
+// Your code goes here
+
+	<ul class="Container">
+	  <li class="Node IsRoot ExpandClosed">
+	    <div class="Expand"></div>
+	    <div class="Content">Films</div>
+	  </li>
+	</ul>
+
+rootNode.appendChild(/* Append your TreeView node*/);
