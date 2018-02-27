91109b57bf5d4e938ff691fab1ca9d6d737e2b03
RomanCook <cookr@ukr.net>
2018-02-27 23:50:26 +0200

    hw11

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,12 +1,67 @@
-var rootNode = document.getElementById("root");
+processTree(document.getElementById('root'), structure);
 
-// Your code goes here
+function processTree (parent, array) {
+  array.forEach(function (node) {
+    if (node.folder) {
+      addFolder(parent, node.title, node.children);
+    } else {
+      addFile(parent, node.title);
+    }
+  })
+}
 
-	<ul class="Container">
-	  <li class="Node IsRoot ExpandClosed">
-	    <div class="Expand"></div>
-	    <div class="Content">Films</div>
-	  </li>
-	</ul>
+function addFolder (node, title, children) {
+  const container = document.createElement('div');
+  const childrenContainer = document.createElement('div');
+  const line = document.createElement('div');
+  const icon = document.createElement('i');
+  const label = document.createElement('span');
+  childrenContainer.className = 'children hidden';
+  icon.className = 'material-icons folder';
+  icon.textContent = 'folder';
+  label.textContent = title;
+  line.className = 'line';
+  line.closed = true;
+  line.onclick = toggleFolder.bind(line, icon, childrenContainer);
+  line.appendChild(icon);
+  line.appendChild(label);
+  if (children) {
+    processTree(childrenContainer, children);
+  } else {
+    const empty = document.createElement('div');
+    const sign = document.createElement('span');
+    sign.textContent = 'Folder is empty';
+    sign.className = 'italic';
+    empty.className = 'line';
+    empty.appendChild(sign);
+    childrenContainer.appendChild(empty);
+  }
+  container.appendChild(line);
+  container.appendChild(childrenContainer);
+  node.appendChild(container);
+}
 
-rootNode.appendChild(/* Append your TreeView node*/);
+function addFile (node, title) {
+  const line = document.createElement('div');
+  const icon = document.createElement('i');
+  const label = document.createElement('span');
+  icon.className = 'material-icons file';
+  icon.textContent = 'insert_drive_file';
+  label.textContent = title;
+  line.className = 'line';
+  line.appendChild(icon);
+  line.appendChild(label);
+  node.appendChild(line);
+}
+
+function toggleFolder (icon, childrenContainer) {
+  if (this.closed) {
+    icon.textContent = 'folder_open';
+    childrenContainer.className = 'children';
+    this.closed = false;
+  } else {
+    icon.textContent = 'folder';
+    childrenContainer.className = 'children hidden';
+    this.closed = true;
+  }
+}
